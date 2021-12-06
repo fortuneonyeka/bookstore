72e471618b9cca739a46a6c03528873200025a49
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-06 20:45:41 +0100

    add provider and Router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
+import NavBar from "./components/NavBar";
+import Books from "./pages/books";
+import Categories from "./pages/categories";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/">
+      <NavBar />
+      <Provider store={store}>
+        <Switch>
+          <Route exact path="/">
+            <Books />
+          </Route>
+          <Route exact path="/categories">
+            <Categories />
+          </Route>
+        </Switch>
+      </Provider>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
