9796d8a08521e5957021703b046d29f2b9e00026
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-06 21:37:38 +0100

    add reviewers recommendations

--- src/redux/books/initialbookstate.js
+++ src/redux/books/initialbookstate.js
@@ -1,34 +1,3 @@
-const initialBookState = [
-  // {
-  //   author: 'Chinua Achebe',
-  //   title: 'Things Fall Apart',
-  //   id: 1,
-  // },
-  // {
-  //   author: 'Hans Christian Andersen',
-  //   title: 'Fairy tales',
-  //   id: 2,
-  // },
-  // {
-  //   author: 'Dante Alighieri',
-  //   title: 'The Divine Comedy',
-  //   id: 3,
-  // },
-  // {
-  //   author: 'Chinua Achebe',
-  //   title: 'Anthills of the savanah',
-  //   id: 4,
-  // },
-  // {
-  //   author: 'Unknown',
-  //   title: 'Around the globe',
-  //   id: 5,
-  // },
-  // {
-  //   author: 'Unknown',
-  //   title: 'One Thousand and One Nights',
-  //   id: 6,
-  // },
-];
+const initialBookState = [{}];
 
 export default initialBookState;
