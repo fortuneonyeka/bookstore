d38523b1a964a0774f4c8fc14d48a42b5a147328
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-06 20:49:03 +0100

    add project initial states

---
+++ src/redux/books/initialbookstate.js
@@ -0,0 +1,34 @@
+const initialBookState = [
+  {
+    author: "Chinua Achebe",
+    title: "Things Fall Apart",
+    id: 1,
+  },
+  {
+    author: "Hans Christian Andersen",
+    title: "Fairy tales",
+    id: 2,
+  },
+  {
+    author: "Dante Alighieri",
+    title: "The Divine Comedy",
+    id: 3,
+  },
+  {
+    author: "Chinua Achebe",
+    title: "Anthills of the savanah",
+    id: 4,
+  },
+  {
+    author: "Unknown",
+    title: "Around the globe",
+    id: 5,
+  },
+  {
+    author: "Unknown",
+    title: "One Thousand and One Nights",
+    id: 6,
+  },
+];
+
+export default initialBookState;
