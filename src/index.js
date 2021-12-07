1969fcc9168aab859ddf57be31442bd2c72ed5e5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-07 18:30:17 +0100

    import App.css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
