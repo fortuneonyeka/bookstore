35c58922fb41a3252c91cd8d7ec0229af350ebd2
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-07 18:29:11 +0100

    update initialbookstate

--- src/redux/books/initialbookstate.js
+++ src/redux/books/initialbookstate.js
@@ -1,3 +1,24 @@
-const initialBookState = [{}];
+const initialBookState = [
+  {
+    bookAuthor: '',
+    bookTitle: '',
+    id: '',
+  },
+  {
+    bookAuthor: 'Hans Christian Andersen',
+    bookTitle: 'Fairy tales',
+    id: '1836',
+  },
+  {
+    bookAuthor: 'Dante Alighieri',
+    bookTitle: 'The Divine Comedy',
+    id: '1315',
+  },
+  {
+    bookAuthor: 'Unknown',
+    bookTitle: 'The Epic Of Gilgamesh',
+    id: '-1700',
+  },
+];
 
 export default initialBookState;
